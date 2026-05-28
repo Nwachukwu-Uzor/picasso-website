9805c31affd6b3d0f4770985b503eca66bc437fd
¨Nwachukwu-Uzor¨ <uzornwachukwu.e.@gmail.com>
2026-05-28 13:22:20 +0100

    add: scroll based animation between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { TwentySixAbout } from "./components/26-about-section";
 import { Footer } from "./components/footer";
 import { Header } from "./components/header";
@@ -6,17 +7,38 @@ import { ImageShow } from "./components/image-show";
 import { Songs } from "./components/songs";
 import { useConfetti } from "./hooks/useConfetti";
 
+const SectionReveal = ({ children, delay = 0 }: { children: React.ReactNode; delay?: number }) => (
+  <motion.div
+    initial={{ opacity: 0, y: 60 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    viewport={{ once: true, amount: 0.08 }}
+    transition={{ duration: 0.7, ease: "easeOut", delay }}
+  >
+    {children}
+  </motion.div>
+);
+
 function App() {
   useConfetti(20000); // TODO: restore to 1 * 60 * 1000
 
   return (
     <>
       <Header />
-      <Hero />
-      <TwentySixAbout />
-      <ImageShow />
-      <Songs />
-      <Footer />
+      <SectionReveal>
+        <Hero />
+      </SectionReveal>
+      <SectionReveal>
+        <TwentySixAbout />
+      </SectionReveal>
+      <SectionReveal>
+        <ImageShow />
+      </SectionReveal>
+      <SectionReveal>
+        <Songs />
+      </SectionReveal>
+      <SectionReveal>
+        <Footer />
+      </SectionReveal>
     </>
   );
 }
